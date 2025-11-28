a04087a1cff70e76251e7fc2e0388c9cf1ddbeea
Brattynn <brattynndev@icloud.com>
2025-11-28 01:07:40 -0500

    Initial Commit

--- src/main/java/com/pd/Game.java
+++ src/main/java/com/pd/Game.java
@@ -1,17 +1,103 @@
 package com.pd;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.awt.*;
+import java.awt.image.BufferStrategy;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+//Main class
+public class Game extends Canvas implements Runnable {
+
+    private static final long serialVersionUID = 1550691097823471818L;
+
+    //Create the Aspect Ratio
+    public static final int WIDTH = 640;
+    public static final int HEIGHT =  WIDTH / 12 * 9;
+
+    private Thread thread;
+    private boolean running = false;
+
+    public Game() {
+        new Window(WIDTH, HEIGHT, "Game Build", this);
+    }
+
+    public synchronized void start() {
+        thread = new Thread(this);
+        thread.start();
+        running = true;
+    }
+
+    public synchronized void stop() {
+        try {
+            thread.join();
+            running = false;
         }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    //The Game Loop
+    public void run() {
+        long lastTime = System.nanoTime();			//Timestamp of the last loop iteration
+        double amountOfTicks = 60.0;				//Targets 60 game logic updates per second
+        double ns = 1000000000 / amountOfTicks;		//How many nanoseconds should pass between ticks
+        double delta = 0;							//Accumulates time to know when to tick
+        long timer = System.currentTimeMillis();	//Used to count one second intervals
+        int frames = 0;								//Counts how many frames were rendered this second
+
+        //This part checks how much real time has passed since the last frame and adds it into delta.
+        //If enough time has accumulated to equal 1 tick’s worth, then it runs tick().
+        while(running) {
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+
+            //This does exactly 60 ticks per second because a tick only runs when enough time has passed.
+            //If the game lags, delta becomes >1, so it runs multiple tick() calls to catch up.
+            while(delta >= 1) {
+                tick();
+                delta --;
+            }
+
+            //This does exactly 60 ticks per second because a tick only runs when enough time has passed.
+            //If the game lags, delta becomes >1, so it runs multiple tick() calls to catch up.
+            if(running) {
+                render();
+            }
+            frames++;
+
+            //Every 1 second, prints how many frames were rendered.
+            //Does not affect the game loop — just for debugging.
+            if (System.currentTimeMillis() - timer > 1000) {
+                timer += 1000;
+                System.out.println("FPS: " + frames);
+                frames = 0;
+            }
+        }
+        stop();
+    }
+
+    private void tick() {
+
+    }
+
+    private void render() {
+        BufferStrategy bs = this.getBufferStrategy();
+        if (bs == null) {
+            this.createBufferStrategy(3);
+            return;
+        }
+
+        Graphics g = bs.getDrawGraphics();
+
+        g.setColor(Color.green);
+        g.fillRect(0, 0, WIDTH, HEIGHT);
+
+        g.dispose();
+        bs.show();
+    }
+
+    public static void main(String[] args) {
+
+        new Game();
     }
 }
--- src/main/java/com/pd/Window.java
+++ src/main/java/com/pd/Window.java
@@ -1,4 +1,30 @@
 package com.pd;
 
-public class Window {
+import java.awt.Canvas;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+//Purpose for this class: To create the game window
+
+public class Window extends Canvas{
+
+    private static final long serialVersionUID = -240840600533728354L;
+
+    public Window(int width, int height, String title, Game game) {
+        JFrame frame = new JFrame(title);
+
+        frame.setPreferredSize(new Dimension(width, height));
+        frame.setMaximumSize(new Dimension(width, height));
+        frame.setMinimumSize(new Dimension(width, height));
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	//Stops the game
+        frame.setResizable(false);	//Allows no resizing of the game window
+        frame.setLocationRelativeTo(null); //Sets the game to the center of the screen
+        frame.add(game);	//Adds the game class into the frame
+        frame.setVisible(true);
+        game.start();
+    }
+
+
 }
